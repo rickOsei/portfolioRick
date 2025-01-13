4ecaa958576731dfaee32f94d9193f9425117a47
Osei Rick <rickoseiampah@gmail.com>
2025-01-13 21:19:20 +0000

    comment removed

--- src/components/Skills-Two.tsx
+++ src/components/Skills-Two.tsx
@@ -26,24 +26,6 @@ const SkillsTwo = () => {
           }
         ></div>
       </div>
-
-      {/* <div
-        className={
-          currentMode === "dark"
-            ? "skills-right-mini w-full px-2 flex flex-row justify-center items-center py-5 bg-bgColorDark space-x-8 lg:hidden"
-            : "skills-right-mini w-full px-2 flex flex-row justify-center items-center py-5 bg-bgColorLight space-x-8 lg:hidden"
-        }
-      >
-       
-        <SiTailwindcss className={currentMode === "dark" ? "dark-text" : ""} />
-        <SiMui className={currentMode === "dark" ? "dark-text" : ""} />
-        <DiPhotoshop className={currentMode === "dark" ? "dark-text" : ""} />
-        <SiRedux className={currentMode === "dark" ? "dark-text" : ""} />
-        <TbBrandFirebase
-          className={currentMode === "dark" ? "dark-text" : ""}
-        />
-        <div className="horizontal-line"></div>
-      </div> */}
     </>
   );
 };
